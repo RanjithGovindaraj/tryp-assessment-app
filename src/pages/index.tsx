468618eb24aea447e6c0aa8b62833877ae72fcc2
Ranjith G <kmgrranjith@gmail.com>
2023-07-15 20:20:47 +0530

    Add Datatable & mock data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import DataTable from "@/components/DataTable";
 
 export default function Home() {
   return (
@@ -11,8 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        Hello world
+        <DataTable />
       </main>
     </>
-  )
+  );
 }
