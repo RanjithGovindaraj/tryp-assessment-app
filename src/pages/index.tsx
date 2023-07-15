c7dd5f321d7a04275e729a7815239d667f34b3b2
Ranjith G <kmgrranjith@gmail.com>
2023-07-15 18:43:51 +0530

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tryp Datatable</title>
+        <meta name="description" content="Application created for tryp assessment" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main}`}>
+        Hello world
+      </main>
+    </>
+  )
+}
